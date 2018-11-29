96eb27f0efb0639e85c9b1a2be74d3743eec5b96
Ignacio Oliver <ignacio.oliver@gmail.com>
2018-11-29 14:04:22 -0300

    Primera versión

---
+++ test/integration/user_filters_issues_list_spec.js
@@ -0,0 +1,51 @@
+describe('issues list', function() {
+  it('shows all issues', function() {
+    cy.fixture({
+      title: 'lorem ipsum'
+    });
+    cy.fixture({
+      title: 'foo bar',
+      status: 'closed'
+    });
+
+    cy.visit('/issues');
+
+    cy.contains('lorem ipsum').contains('open').should('exist');
+    cy.contains('foo bar').contains('closed').should('exist');
+  });
+
+  it('show open issues', function(){
+    cy.fixture({
+      title: 'lorem ipsum',
+      status: 'open'
+    });
+    cy.fixture({
+      title: 'foo bar',
+      status: 'closed'
+    });
+
+    cy.visit('/issues');
+
+    cy.contains('Open issues').click();
+    cy.contains('open').should('exist');
+    cy.contains('closed').should('not.exist');
+  });
+
+  it('show closed issues', function(){
+    cy.fixture({
+      title: 'lorem ipsum',
+      status: 'open'
+    });
+    cy.fixture({
+      title: 'foo bar',
+      status: 'closed'
+    });
+
+    cy.visit('/issues');
+
+    cy.contains('Closed issues').click();
+    cy.contains('closed').should('exist');
+    cy.contains('open').should('not.exist');
+  });
+
+});
